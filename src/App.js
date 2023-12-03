a081878ecfc022ac35a6422dd3bd2175f6c870ca
Abhishek Choudhary <abhishek.choudhary@cogoport.com>
2023-12-03 05:39:28 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 // import Register from "./pages/register/Register"
-// import Home from "./pages/home/Home"
-import Profile from "./pages/profile/Profile";
+import Home from "./pages/home/Home"
+// import Profile from "./pages/profile/Profile";
 // import Login from "./pages/login/Login";
 
 function App() {
   return (
     <div>
-        {/* <Home /> */}
-        <Profile />
+        <Home />
+        {/* <Profile /> */}
         {/* <Login /> */}
         {/* <Register /> */}
     </div>
